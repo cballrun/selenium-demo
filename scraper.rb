826d5356df416eb1be09f17c668dbeb3a756b0ba
cballrun <carter.k.ball@gmail.com>
2024-02-29 14:41:38 -0500

    Selenium setup, html_products returning array of Selenium elements

--- scraper.rb
+++ scraper.rb
@@ -1,1 +1,16 @@
 require "selenium-webdriver"
+require 'pry'
+
+PokemonProduct = Struct.new(:url, :image, :name, :price)
+
+pokmemon_products = []
+
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument("--headless")
+
+driver = Selenium::WebDriver.for :chrome, options: options
+
+driver.navigate.to "https://scrapeme.live/shop/"
+
+html_products = driver.find_elements(:css, "li.product") #find all css elements that are li.product
+
