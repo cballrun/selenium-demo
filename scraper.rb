93e019f68d2e607e4cdbd0ffcf94d5a3aa46ed23
cballrun <carter.k.ball@gmail.com>
2024-03-01 10:25:06 -0500

    Application creating array of 16 PokemonProducts from one page of site

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 PokemonProduct = Struct.new(:url, :image, :name, :price)
 
-pokmemon_products = []
+pokemon_products = []
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument("--headless")
@@ -13,4 +13,16 @@ driver = Selenium::WebDriver.for :chrome, options: options
 driver.navigate.to "https://scrapeme.live/shop/"
 
 html_products = driver.find_elements(:css, "li.product") #find all css elements that are li.product
+
+html_products.each do |html_product|
+    url = html_product.find_element(:css, "a").attribute("href")
+    image = html_product.find_element(:css, "img").attribute("src")
+    name = price = html_product.find_element(:css, "h2").text
+    price = html_product.find_element(:css, "span").text
+
+    pokemon_product = PokemonProduct.new(url, image, name, price)
+    pokemon_products << pokemon_product
+end
+
+driver.quit
 
